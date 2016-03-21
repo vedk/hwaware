d9e54a4f552bb839a00e2ee50df712a1ca02a5d4
Ved Khandekar <vedk90@gmail.com>
2016-03-21 06:02:23 +0530

    Added classes dict in index.py for extending the <select> in index.html

--- index.py
+++ index.py
@@ -4,6 +4,12 @@ from hwaware import app
 from flask import render_template
 from time import strftime
 
+classes = {'10F': '10th Faith',
+           '10C': '10th Courage',
+           '10H': '10th Hope',
+           '9F' : '9th Faith',
+           '9H' : '9th Hope'}
+
 @app.route('/')
 def on_index():
     y = int(strftime('%Y'))
@@ -12,4 +18,4 @@ def on_index():
     else:
         footer = '&copy; 2016 - ' + str(y) + ' Ved Khandekar'
 
-    return render_template('index.html', footertext=footer)
+    return render_template('index.html', footertext=footer, classes_data=classes)
