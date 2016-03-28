20f45c48e04e6de66ea685826682904789fa0e77
Ved Khandekar <vedk90@gmail.com>
2016-03-28 05:40:50 +0530

    Changed the date to 7:15am 15 June, 2015

--- about.py
+++ about.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 @app.route('/about')
 def on_about():
-    then = datetime(2016, 3, 21, 5, 15, 46)
+    then = datetime(2015, 6, 15, 7, 15)
     secondspast = (datetime.now() - then).total_seconds()
     
     return render_template('about.html', longness=secondspast)
